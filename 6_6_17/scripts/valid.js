577827d50b074366ea5c51695bc9a4830e57056b
Jonathan Hagen <jonathan.i.hagen@gmail.com>
2017-06-08 16:26:51 -0500

    Visual Improvements
    This update includes a number of changes that streamline the webpage.
    
    CL 6617
    - Sections updated with new margins and padding to better fit tablet/smartphone screens. 
    - Resume section redesign. New image and PDF download link.
    - Contact Me section populated. JavaScript function is currently incorporated inside the index file in order to check for the email field being left blank or invalid.
    - Responsive improvements to multiple elements throughout the page.

---
+++ 6_6_17/scripts/valid.js
@@ -0,0 +1,9 @@
+// Code for validating the form
+// Visit http://www.javascript-coder.com/html-form/javascript-form-validation.phtml
+// for details
+var frmvalidator  = new Validator("contactform");
+frmvalidator.addValidation("name","req","Please provide your name"); 
+frmvalidator.addValidation("email","req","Please provide your email"); 
+frmvalidator.addValidation("email","email","Please enter a valid email address"); 
+frmvalidator.addValidation("email","email","Please enter a valid email address"); 
+		
